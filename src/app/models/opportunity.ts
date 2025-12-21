c978e6e6228ac612930a20f5efe83ff60985ddda
YOUSSEF EL BOUNDATI <elboundati1234@gmail.com>
2025-12-21 16:30:24 +0100

    update

--- src/app/models/opportunity.ts
+++ src/app/models/opportunity.ts
@@ -3,6 +3,7 @@ export interface Opportunity {
   title: string;
   type: 'Scholarship' | 'Training' | 'Event';
   organization: string;
+  orgDescription: string; // Nouveau champ pour la description dynamique
   location: string;
   updatedAt: string;
   imageUrl: string;
