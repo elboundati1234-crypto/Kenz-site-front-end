853d8b174472b787077f15c9c5c66440622eddda
YOUSSEF EL BOUNDATI <elboundati1234@gmail.com>
2026-01-04 19:37:00 +0100

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './pages/header/header';
-import { Footer } from './pages/footer/footer';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+
+
 
 @Component({
   selector: 'app-root',
