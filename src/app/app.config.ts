cb246e26805e90056728b2663ba2ce8b5a2b134b
YOUSSEF EL BOUNDATI <elboundati1234@gmail.com>
2026-01-17 22:02:23 +0100

    update event

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+
+// 1. IMPORTS NÉCESSAIRES POUR LA DATE FR
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+// 2. ENREGISTREMENT DE LA LANGUE
+registerLocaleData(localeFr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,9 +16,11 @@ export const appConfig: ApplicationConfig = {
       routes,
       withComponentInputBinding(),
       withViewTransitions(),
-     
       withRouterConfig({ onSameUrlNavigation: 'reload' }) 
     ),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), // <-- J'ai ajouté la virgule ici qui manquait
+    
+    // 3. CONFIGURATION GLOBALE
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 };
